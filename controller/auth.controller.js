4f8f77e6f27d6bf4bb705f98608494dde987f365
AyushDubey63 <dubeydestroyer@gamil.com>
2023-12-12 09:39:08 +0530

    forth commit

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,30 +1,49 @@
 import { User } from "../model/Auth.js";
+import bcrypt from 'bcrypt'
 
 export const registerUser = async (req, res) => {
-  const data  = new User(req.body)
+  const { username, password,userType } = req.body;
+
   try {
-    await data.save();
-    res.status(201).json(data)
+    // Hash the password using bcrypt
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      username: username,
+      password: hashedPassword,
+      userType: userType
+    });
+
+    await user.save();
+
+    user.password = undefined;
+
+    res.status(201).json(user);
   } catch (error) {
-    res.status(400).json(error)
-    console.log(error)
+    console.error(error);
+    res.status(400).json({ error: 'Failed to register user' });
   }
-}
+};
 
-export const loginUser = async(req, res) => {
-  const { username, password } = req.body
+export const loginUser = async (req, res) => {
+  const { username, password } = req.body;
 
-  const user = await User.findOne({
-    username
-  })
-  if (!user) {
-    return res.status(401).json({ message: 'Invalid username or password' });
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (passwordMatch) {
+      res.status(200).json({ message: 'Login successful' });
+    } else {
+      res.status(401).json({ message: 'Invalid username or password' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
   }
-
-  if (user && user.password === password) {
-    res.status(200).json({ message: 'Login successful' });
-  } else {
-    res.status(401).json({ message: 'Invalid username or password' });
-  }
-
-}
+};
